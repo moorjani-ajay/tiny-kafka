5372db05ffc5121fc548b4f939e923ae4148fced
ajay.moorjani <moorjaniajay@gmail.com>
2020-07-13 10:53:45 +0800

    First Commit

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Script for automatic setup Kafka server
+#
+# DO NOT RUN THIS SCRIPT ON YOUR PC OR MAC!
+#
+# The latest version of this script is available at:
+# https://github.com/moorjani-ajay/tiny-kafka
+#
+sudo apt-get install openjdk-8-jdk
